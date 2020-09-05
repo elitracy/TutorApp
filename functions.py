f768ab1fe22ac316d937fdb6de32264246c249db
Benjamin Nguyen <bnguyen1221@tamu.edu>
2020-09-05 18:36:18 -0500

    Revert "Merge branch 'master' of https://github.com/elitracy/TutorApp"
    This reverts commit 9c74dbfb8c9e1afd264ae225083994257fa549f3, reversing
    changes made to c32db4edba66d153eeb64f6b6285afec358f7df3.

--- functions.py
+++ functions.py
@@ -1,6 +1,11 @@
 import json
 
-def classSearch(className,userType):
+def compareClasses(studentName,studentClasses):
+
+    #get student name
+    #get their classes
+    #compare against all tutors with same classes
+    #return tutors that have those classes
 
     with open('studentData.json','r+') as file:
         studentData = json.load(file)
@@ -10,128 +15,19 @@ def classSearch(className,userType):
         tutorData = json.load(file)
         file.close()
 
-    #student searches for tutor 
-    if(userType == 'student'):
 
-        for i in tutorData['users']:
-            for j in i['classes']:
-                if(j == className):
-                    print(i['name'])
-
-    elif(userType == 'tutor'):
-        for i in studentData['users']:
-            for j in i['classes']:
-                if(j == className):
-                    print(i['name'])
-
-def nameSearch(name,userType):
-
-    with open('studentData.json','r+') as file:
-        studentData = json.load(file)
-        file.close()
-
-    with open('tutorData.json','r+') as file:
-        tutorData = json.load(file)
-        file.close()
-
-    correctUser = 'None'
-     #tutor searching for student
-    if(userType == 'tutor'):
-        for i in studentData['users']:
-            if(i['name'] == name):
-                correctUser = i
-                print("User Found")
-            # else:
-            #     print("User not Found")
-
-    elif(userType == 'student'):
-        for i in tutorData['users']:
-            if(i['name'] == name):
-                correctUser = i
-                print('User Found')
-            # else:
-            #     print("User not Found")
-    
-def tutorLogin(username,password):
-
-    with open('tutorData.json','r+') as file:
-        tutorData = json.load(file)
-        file.close()
-
-    for i in tutorData['users']:
-        if(i['username'] == username and i['password'] == password):
-            print('Login Successful\n')
-            return True
-        else:
-            print('Invalid Credentials\n')
-            return False
-
-def studentLogin(username,password):
-    
-    with open('studentData.json','r+') as file:
-        studentData = json.load(file)
-        file.close()
-
+    #find correct student data
     for i in studentData['users']:
-        if(i['username'] == username and i['password'] == password):
-            print('Login Successful\n')
-            return True
-        else:
-            print('Invalid Credentials')
-            return False
+        if(i['name'] == studentName):
+            correctUser = i
 
-def createAccount(userType):
-    if(userType == "student"):
+    #compare class student needs to all tutors
+    for i in tutorData['users']:
+        for j in i['classes']:
+            if(j == studentClasses):
+                print(i['name'])
         
-        username = input("Username: ")
-        password = input("Password: ")
-        studentName = input("Name: ")
-        studentPriceMax = input("Max Price/hour: $")
-        studentClasses = input("Classes You Need Help In: ").split()
+        
+        
 
-        userInfo = {
-            'name' : studentName,
-            'price' : studentPriceMax,
-            'classes' : studentClasses,
-            'username' : username,
-            'password' : password
-        }
-
-        with open('studentData.json', 'r+') as file:
-            data={}
-            data = json.load(file)
-            file.seek(0)
-            data['users'].append(userInfo)
-            json.dump(data,file,indent=4)
-            file.close()
-
-    if(userType == "tutor"):
-        username = input("Username: ")
-        password = input("Password: ")
-        tutorName = input("Name: ")
-        tutorPrice = input("Price/hour: $")
-        tutorLocations = input("Locations: ").split()
-        tutorMajor = input("Major: ")
-        tutorGPA = input("GPA: ")
-        tutorClasses = input("Classes: ").split()
-
-        userInfo = {
-            'name' : tutorName,
-            'price' : tutorPrice,
-            'locations' : tutorLocations,
-            'major' : tutorMajor,
-            'gpa' : tutorGPA,
-            'classes' : tutorClasses,
-            'username' : username,
-            'password' : password
-        }
-
-        with open('tutorData.json', 'r+') as file:
-            data={}
-            data = json.load(file)
-            file.seek(0)
-            data['users'].append(userInfo)
-            json.dump(data,file,indent=4)
-            file.close()
-
-
+compareClasses('Eli','CSCE121')
--- main.py
+++ main.py
@@ -1,73 +1,58 @@
-import functions
+import json
+
+userInput = input("Student or Tutor: ")
+
+tutorUsers = {}
+tutorUsers['users'] = []
 
 
-
-userInput = ''
-validUser = False
-userType = ''
-
-while(userInput != 'quit' or userInput != 'q'):
-
-    #allow user to quit application
-    if(userInput == 'q' or userInput == 'quit'):
-        break
-
-    if(validUser == False):
-        #if user is not logged in 
-        userInput = input("Login or Create Account: ")
-        print()
-    else:
-
-        #search for users based on user type 
-        if(userType == 'student'):
-            #students search for tutors
-            search = input("Search for Tutors by Name or Class: ")
-            print()
-
-            functions.classSearch(search,'student')
-            functions.nameSearch(search,'student')
-
-        elif(userType == 'tutor'):
-            #tutors search for students
-            search = input("Search for Students by Name or Class: ")
-            print()
-            functions.classSearch(search,'tutor')
-            functions.nameSearch(search,'tutor')
-
-    #user login
-    if(userInput == 'login'.casefold()):
-        userInput = input("Student or Tutor: ")
-        print()
-
-        #login as a student
-        if(userInput == 'student'.casefold()):
-            username = input("Username: ")
-            password = input("Password: ")
-            validUser = functions.studentLogin(username,password)
-            print(validUser)
-            userType = 'student'
-
-        #login as a tutor
-        if(userInput == 'tutor'.casefold()):
-            username = input("Username: ")
-            password = input("Password: ")
-            validUser = functions.tutorLogin(username,password)
-            userType = 'tutor'
-
-    if(userInput == 'create account'.casefold()):
-        userInput = input("Student or Tutor: ")
-        print()
-
-        if(userInput == "student".casefold()):
-            functions.createAccount('student')
-            validUser = True
-            userType = 'student'
-
-        elif(userInput == "tutor".casefold()):
-            functions.createAccount('tutor')
-            validUser = True
-            userType = 'tutor'
+if(userInput == "student".casefold()):
 
     
 
-    
+    studentName = input("Name: ")
+    studentPriceMax = input("Max Price/hour: ")
+    studentClasses = input("Classes You Need Help In: ").split()
+
+    userInfo = {
+        'name' : studentName,
+        'price' : studentPriceMax,
+        'classes' : studentClasses
+    }
+
+    with open('studentData.json', 'r+') as file:
+        data={}
+        data = json.load(file)
+        file.seek(0)
+        data['users'].append(userInfo)
+        json.dump(data,file,indent=4)
+        file.close()
+
+
+elif(userInput == "tutor".casefold()):
+    tutorName = input("Name: ")
+    tutorPrice = input("Price/hour: ")
+    tutorLocations = input("Locations: ").split()
+    tutorMajor = input("Major: ")
+    tutorGPA = input("GPA: ")
+    tutorClasses = input("Classes: ").split()
+
+    userInfo = {
+        'name' : tutorName,
+        'price' : tutorPrice,
+        'locations' : tutorLocations,
+        'major' : tutorMajor,
+        'gpa' : tutorGPA,
+        'classes' : tutorClasses
+    }
+
+    with open('tutorData.json', 'r+') as file:
+        data={}
+        data = json.load(file)
+        file.seek(0)
+        data['users'].append(userInfo)
+        json.dump(data,file,indent=4)
+        file.close()
+
+else:
+    print("invalid input")
